5f61d4a59310968aea50ccdf64a116e3a485b134
ambit <ambit_tsai@qq.com>
2019-01-05 22:32:22 +0800

    fix: modify `Home`'s style and text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,23 @@ export default function Home() {
 	return (
 		<div className={style['Home']}>
 			<div className={style['Home__content']}>
-				<h2><Icon type="info-circle-o" /> Technology Stack</h2>
+				<h3><Icon type="info-circle-o" /> Technology Stack</h3>
 				<ul>
 					<li>1. <b>ECMAScript 6</b></li>
-					<li>2. <b>Webpack:</b> the module bundler</li>
-					<li>3. <b>Babel:</b> the JavaScript compiler</li>
-					<li>4. <b>ESLint:</b> the JavaScript linter</li>
-					<li>5. <b>React 16:</b> use the React-like library `anujs` instead</li>
-					<li>6. <b>Reach:</b> use the compatible version provided by `anujs`</li>
-					<li>7. <b>Rematch:</b> use the compatible version provided by `anujs`</li>
-					<li>8. <b>Ant Design 1.x:</b> the front-end component library</li>
+					<li>2. <b>Webpack:</b> The module bundler</li>
+					<li>3. <b>Babel:</b> The JavaScript compiler</li>
+					<li>4. <b>ESLint:</b> The JavaScript linter</li>
+					<li>5. <b>React 16:</b> Use the React-like library `anujs` instead</li>
+					<li>6. <b>Reach:</b> Use the compatible version provided by `anujs`</li>
+					<li>7. <b>Rematch:</b> Use the compatible version provided by `anujs`</li>
+					<li>8. <b>Ant Design 1.x:</b> The React UI framework</li>
 				</ul>
-				<h2><Icon type="info-circle-o" /> Others</h2>
+				<h3><Icon type="info-circle-o" /> Others</h3>
 				<ul>
 					<li>1. <b>ES6 Module</b> </li>
 					<li>2. <b>CSS Modules</b> </li>
-					<li>3. <b>Class Properties:</b> use babel plugin to support this feature</li>
-					<li>4. <b>Code Splitting:</b> use the `import()` syntax</li>
+					<li>3. <b>Class Properties:</b> Use babel plugin to support this feature</li>
+					<li>4. <b>Code Splitting:</b> Use the dynamic `import()` syntax</li>
 				</ul>
 			</div>
 		</div>
